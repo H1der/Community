72de31f1dd2e2bdc1d6e00dd22340105d972a153
Hider <724598894@qq.com>
2019-07-08 22:00:42 +0800

    从 Session 中取出 user 信息

--- src/main/java/com/hider/community/community/controlloer/AuthorizeController.java
+++ src/main/java/com/hider/community/community/controlloer/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -23,7 +25,9 @@ public class AuthorizeController {
     private String redirectUri;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
@@ -32,7 +36,14 @@ public class AuthorizeController {
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            //登录成功
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            //登录失败
+            return "redirect:/";
+        }
+
     }
 }
