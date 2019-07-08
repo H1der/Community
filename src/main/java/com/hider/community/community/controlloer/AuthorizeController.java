09f57bae774ae769c3085f3540e62ed24220cfe6
Hider <724598894@qq.com>
2019-07-08 21:24:11 +0800

    使用配置文件存储 github key

--- src/main/java/com/hider/community/community/controlloer/AuthorizeController.java
+++ src/main/java/com/hider/community/community/controlloer/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.hider.community.community.dto.AccessTokenDto;
 import com.hider.community.community.dto.GithubUser;
 import com.hider.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,13 +15,20 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-        accessTokenDto.setClient_id("fca1c60250ad40aff6d6");
-        accessTokenDto.setClient_secret("33d1ced2f0d28fcebef0ea3f312a2085873705b2");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
         GithubUser user = githubProvider.getUser(accessToken);
