9f857d0d5946d626acacb214b235f0329cf06db8
Hider <724598894@qq.com>
2019-07-12 22:43:17 +0800

    抽取 Session 验证公共模块

--- src/main/java/com/hider/community/community/controller/IndexController.java
+++ src/main/java/com/hider/community/community/controller/IndexController.java
@@ -1,9 +1,6 @@
 package com.hider.community.community.controller;
 
 import com.hider.community.community.dto.PaginationDto;
-import com.hider.community.community.dto.QuestionDto;
-import com.hider.community.community.mapper.UserMapper;
-import com.hider.community.community.model.User;
 import com.hider.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,16 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
-
-
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
@@ -32,20 +23,6 @@ public class IndexController {
                         @RequestParam(name = "size", defaultValue = "5") Integer size
 
     ) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-
         PaginationDto pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
--- src/main/java/com/hider/community/community/controller/PublishController.java
+++ src/main/java/com/hider/community/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package com.hider.community.community.controller;
 
 import com.hider.community.community.mapper.QuestionMapper;
-import com.hider.community.community.mapper.UserMapper;
 import com.hider.community.community.model.Question;
 import com.hider.community.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -19,8 +17,6 @@ public class PublishController {
 
     @Autowired
     private QuestionMapper questionMapper;
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish() {
@@ -53,24 +49,9 @@ public class PublishController {
             return "publish";
         }
 
-
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-            if (user == null) {
-                model.addAttribute("error", "用户未登录");
-                return "publish";
-            }
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            return "redirect:/";
         }
 
         //接收表单数据,存入数据库
