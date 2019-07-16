bbc9561a5293700a46c23fc759f7ac640691210f
Hider <724598894@qq.com>
2019-07-16 17:06:00 +0800

    异步登录回复的实现

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -15,7 +15,15 @@ function post() {
             if (response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message)
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=fca1c60250ad40aff6d6&redirect_uri=http://localhost:8080/callback&scope=user&state=1")
+                        localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
         },
         dataType: "json"
